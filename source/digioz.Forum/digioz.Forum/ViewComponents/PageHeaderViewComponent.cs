2cbef605d50ed574fa0616d4326e4f9dcfbe0753
DigiOz Multimedia <digioz@gmail.com>
2024-11-27 10:24:21 -0600

    Refactor page header into a separate view component
    Moved page header HTML from `_Layout.cshtml` to a new view component.
    - Removed static HTML from `_Layout.cshtml` and replaced it with an async call to `PageHeader`.
    - Created `default.cshtml` to hold the page header HTML.
    - Added `PageHeaderViewComponent` class in `PageHeaderViewComponent.cs` with an `Invoke` method to return the view.
    
    These changes enhance maintainability and modularity by isolating the page header.

---
+++ source/digioz.Forum/digioz.Forum/ViewComponents/PageHeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace digioz.Forum.ViewComponents
+{
+    public class PageHeaderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
