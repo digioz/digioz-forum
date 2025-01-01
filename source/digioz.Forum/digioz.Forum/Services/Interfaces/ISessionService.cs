1b74dd31a3e7c62847676cd1ec601808f564345c
digioz <digioz@gmail.com>
2025-01-01 16:59:16 -0600

    Add SessionService for managing ForumSession entities
    Introduce SessionService class in digioz.Forum.Services namespace
    implementing ISessionService interface. This class provides methods
    to manage ForumSession entities, including Get, GetAll, Add, Edit,
    and Delete operations. Add ISessionService interface in
    digioz.Forum.Services.Interfaces namespace defining the contract
    for SessionService. Include necessary using directives for
    digioz.Forum.Models, digioz.Forum.Services.Interfaces, and
    Microsoft.Build.Framework.

---
+++ source/digioz.Forum/digioz.Forum/Services/Interfaces/ISessionService.cs
@@ -0,0 +1,13 @@
+﻿using digioz.Forum.Models;
+
+namespace digioz.Forum.Services.Interfaces
+{
+    public interface ISessionService
+    {
+        ForumSession Get(int id);
+        IEnumerable<ForumSession> GetAll();
+        void Add(ForumSession session);
+        void Edit(ForumSession session);
+        void Delete(int id);
+    }
+}
