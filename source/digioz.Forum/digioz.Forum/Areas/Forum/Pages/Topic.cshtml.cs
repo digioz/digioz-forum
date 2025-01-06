ce45af1e43cd6ce703da2b978e434f7c61e8a0cb
digioz <digioz@gmail.com>
2025-01-06 10:05:57 -0600

    Added Session capture logic to all pages code behind.

--- source/digioz.Forum/digioz.Forum/Areas/Forum/Pages/Topic.cshtml.cs
+++ source/digioz.Forum/digioz.Forum/Areas/Forum/Pages/Topic.cshtml.cs
@@ -1,3 +1,5 @@
+using digioz.Forum.Helpers;
+using digioz.Forum.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,24 @@ namespace digioz.Forum.Areas.Forum.Pages
 {
     public class TopicModel : PageModel
     {
+        private readonly IForumSessionService _forumConfigService;
+
+        public TopicModel(IForumSessionService forumConfigService)
+        {
+            _forumConfigService = forumConfigService;
+        }
+
+        private void GetSession()
+        {
+            var sessionId = HttpContext.Session.Id;
+            var pageName = Request.Path;
+            var forumSessionHelper = new ForumSessionHelper(_forumConfigService);
+            forumSessionHelper.AddSession(HttpContext, sessionId, pageName);
+        }
+
         public void OnGet()
         {
+            GetSession();
         }
     }
 }
