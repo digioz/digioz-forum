9d167145e52872dee61c24d72f4d12998961ad24
DigiOz Multimedia <digioz@gmail.com>
2025-07-01 16:25:36 +0000

    Issue #54 - Add migration for Bootstrap forum styles records in database
    Introduces `InsertBootstrapForumStyles` migration to add
    "bootstrap" and "bootstrap_dark" styles to the `ForumStyle`
    table. Includes `Up` and `Down` methods for data insertion
    and removal, along with updates to the `Designer.cs` file
    for proper tracking by Entity Framework Core.

---
+++ source/digioz.Forum/digioz.Forum/Data/Migrations/20250701161402_InsertBootstrapForumStyles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace digioz.Forum.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertBootstrapForumStyles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ForumStyle",
+                columns: new[] { "StyleId", "StyleName", "StyleCopyright", "StyleActive", "StylePath", "BbcodeBitField", "StyleParentId", "StyleParentTree" },
+                columnTypes: new[] { "int", "nvarchar(255)", "nvarchar(255)", "int", "nvarchar(255)", "nvarchar(255)", "int", "nvarchar(255)" },
+                values: new object[,]
+                {
+                    { 3, "bootstrap", "&copy; DigiOz Multimedia, Inc", 0, "bootstrap", "//g=", 0, "" },
+                    { 4, "bootstrap_dark", "&copy; DigiOz Multimedia, Inc", 0, "bootstrap_dark", "//g=", 0, "" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ForumStyle",
+                keyColumn: "StyleId",
+                keyValues: new object[] { 3, 4 });
+        }
+    }
+}
